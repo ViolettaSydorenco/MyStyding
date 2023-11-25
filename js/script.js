04f76134be48212c2c84f6c8be937d0d5a15473b
ViolettaSydorenco <sydorenkovioletta@gmail.com>
2023-11-25 19:22:34 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use sctrict";
+
+//alert('Hello');
+// const result = confirm("Are you here?");
+
+// const answer = prompt("Вам є 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('яке ваше імя?', '');
+answers[1] = prompt('яке ваше фамілія?', '');
+answers[3] = prompt('яке ваше побатькові?' , '');
+
+document.write(answers);
